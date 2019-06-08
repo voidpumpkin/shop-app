7465ee73573bc6a87a12f3983e6d1c522f1ede67
Voidpumpkin <juliuslungys@gmail.com>
2019-06-08 17:39:52 +0300

    feat(browse): display first 10 items

--- src/Browse.js
+++ src/Browse.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 
 class Browse extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { shopItems: null, totalShopItems: null };
+  }
+  componentDidMount() {
+    fetch("http://localhost:3001/browse?limit=10")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          shopItems: data.items,
+          totalShopItems: data.totalItems
+        })
+      );
+  }
   render() {
-    return <h1>Browse</h1>;
+    console.log(this.state)
+    if (this.state.shopItems) {
+      return (
+        <ul>
+          {this.state.shopItems.map(item => (
+            <img src={item.image} alt={item.id} />
+          ))}
+        </ul>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
   }
 }
 
