1e8ed28486b58a481d6d0ebe51db12e7f336c007
Voidpumpkin <juliuslungys@gmail.com>
2019-06-11 22:18:27 +0300

    feat(browse): add pagination

--- src/Browse.js
+++ src/Browse.js
@@ -3,26 +3,55 @@ import React, { Component } from "react";
 class Browse extends Component {
   constructor(props) {
     super(props);
-    this.state = { shopItems: null, totalShopItems: null };
+    this.state = {
+      shopItems: null,
+      totalShopItems: null,
+      pageCount: null,
+      currentPage: 1,
+      itemLimit: 10
+    };
   }
   componentDidMount() {
-    fetch("http://localhost:3001/browse?limit=10")
+    this.fetchPageShopItems();
+  }
+  fetchPageShopItems(page = 1) {
+    let start = this.state.itemLimit * (page - 1);
+    fetch(
+      `http://localhost:3001/browse?start=${start}&limit=${this.state.itemLimit}`
+    )
       .then(response => response.json())
       .then(data =>
         this.setState({
           shopItems: data.items,
-          totalShopItems: data.totalItems
+          totalShopItems: data.totalItems,
+          pageCount: Math.ceil(data.totalItems / this.state.itemLimit)
         })
       );
   }
+
   render() {
     if (this.state.shopItems) {
       return (
-        <ul>
-          {this.state.shopItems.map(item => (
-            <img src={item.image} alt={item.id} />
-          ))}
-        </ul>
+        <div>
+          <ul>
+            {this.state.shopItems.map(item => (
+              <img src={item.image} alt={item.id} key={item.id} />
+            ))}
+          </ul>
+          <ul>
+            {/* FIXME: Will overflow with too many pages, separate component needed */}
+            {[...Array(this.state.pageCount)].map((e, i) => (
+              <button
+                onClick={event =>
+                  this.fetchPageShopItems(event.target.innerText)
+                }
+                key={i + 1}
+              >
+                {i + 1}
+              </button>
+            ))}
+          </ul>
+        </div>
       );
     } else {
       return <h1>Loading...</h1>;
