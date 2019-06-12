cfb0fbecfcdf3f15513265ca13c8e4300d5d2634
Voidpumpkin <juliuslungys@gmail.com>
2019-06-12 17:44:44 +0300

    feat(browse): fix Pagination html elements had key params

--- src/Browse/Pagination.js
+++ src/Browse/Pagination.js
@@ -3,26 +3,13 @@ import React from "react";
 function Pagination({ currentPage }) {
   return (
     <div id="pagination">
-      <a
-        id="previuos-page-button"
-        href={`?page=${currentPage - 1}`}
-        key={currentPage - 1}
-      >
+      <a id="previuos-page-button" href={`?page=${currentPage - 1}`}>
         &lt;
       </a>
-      <a
-        id="current-page-button"
-        href={`?page=${currentPage}`}
-        key={currentPage}
-      >
+      <a id="current-page-button" href={`?page=${currentPage}`}>
         {currentPage}
       </a>
-      <a
-        id="next-page-button"
-        href={`?page=${Number(currentPage) + 1}`}
-        key={Number(currentPage) + 1}
-        disabled
-      >
+      <a id="next-page-button" href={`?page=${Number(currentPage) + 1}`}>
         &gt;
       </a>
     </div>
