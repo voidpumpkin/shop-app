eca98e8e4647d73190d5ffc872e018fc0975f7ff
Voidpumpkin <juliuslungys@gmail.com>
2019-06-12 17:22:38 +0300

    feat(browse): move files into browse folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Browse from "./Browse";
+import Browse from "./Browse/Browse";
 
 class App extends Component {
   render() {
