53f6a750f4630fe3fac1a0d78179dfd8861f581d
Voidpumpkin <juliuslungys@gmail.com>
2019-06-13 20:59:51 +0300

    feat(item): fix item basic functionality

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,41 +1,66 @@
 import React, { Component } from "react";
-import queryString from "query-string";
+// import queryString from "query-string";
 import "./Item.css";
 
 class Item extends Component {
   constructor(props) {
     super(props);
-    const queryStringValues = queryString.parse(this.props.location.search);
-    this.state = {
-    };
+    // const queryStringValues = queryString.parse(this.props.location.search);
+    this.state = {};
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.fetchPageShopItems();
+  }
 
-  //   fetchPageShopItems() {
-  //     let start = this.state.itemLimit * (this.state.currentPage - 1);
-  //     fetch(
-  //       `http://localhost:3001/browse?start=${start}&limit=${this.state.itemLimit}`
-  //     )
-  //       .then(response => response.json())
-  //       .then(data =>
-  //         this.setState({
-  //           shopItems: data.items,
-  //           totalShopItems: data.totalItems,
-  //           pageCount: Math.ceil(data.totalItems / this.state.itemLimit)
-  //         })
-  //       );
-  //   }
+  fetchPageShopItems() {
+    fetch(`http://localhost:3001/item/${this.props.match.params.id}`)
+      .then(response => response.json())
+      .then(data => this.setState({ ...data }));
+  }
 
   render() {
-    if (this.state.shopItems) {
+    if (Object.keys(this.state).length !== 0) {
       return (
         <div id="item-container" className="item-container">
-          <header id="item-header-wrapper" className="item-header-wrapper">
-            <div id="item-header" className="item-header">
-              Item
+          <div id="image-wrapper" className="image-wrapper gold-border">
+            <img
+              id="item-image"
+              className="item-image"
+              src={this.state.image}
+              alt={"item"}
+            />
+          </div>
+          <div
+            id="title-price-wrapper"
+            className="title-price-wrapper gold-border"
+          >
+            <div id="item-title" className="item-title">
+              {this.state.title}
             </div>
-          </header>
+            <div id="item-price" className="item-price">
+              {this.state.price
+                ? this.state.price.amounts.EUR
+                : "Price Upon Request"}
+            </div>
+            <div id="item-measurements" className="item-measurements">
+              <b>Measurements:</b>
+              <br />
+              {this.state.measurements.display}
+            </div>
+          </div>
+          <div
+            id="description-creator-wrapper"
+            className="description-creator-wrapper gold-border"
+          >
+            <div id="item-description" className="item-description">
+              {this.state.description}
+            </div>
+            <div id="item-creator" className="item-creator">
+              <b>Creators: </b>
+              {this.state.creators}
+            </div>
+          </div>
         </div>
       );
     } else {
