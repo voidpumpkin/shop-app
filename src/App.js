44fee903e517db4af59926a96ded81364c5f2563
Voidpumpkin <juliuslungys@gmail.com>
2019-06-14 21:51:58 +0300

    add no match page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Browse from "./Browse/Browse";
 import Item from "./Item/Item";
+import NoMatch from "./SharedComponents/NoMatch";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Route exact path="/" component={Browse} />
-        <Route exact path="/item/:id" component={Item} />
+        <Route path="/browse/" component={Browse} />
+        <Route path="/item/:id" component={Item} />
+        <Route component={NoMatch} />
       </Router>
     );
   }
