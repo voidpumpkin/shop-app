947632ec8ed378c29465c8e4a20984999e399c6a
Voidpumpkin <juliuslungys@gmail.com>
2019-06-16 17:18:11 +0300

    add isFavorite in items on get browse

--- server/routes/browseRouter.js
+++ server/routes/browseRouter.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const cachedItems = require('../data/items.json');
+const express = require("express");
+const cors = require("cors");
+const cachedItems = require("../data/items.json");
+const fs = require("fs");
 
 const browseRouter = express.Router();
 
-const getItems = function (payload) {
-    const start = Number.parseInt(payload.start) || 0;
-    const limit = Number.parseInt(payload.limit) || 9;
-    const items = cachedItems.slice(start, start + limit);
-
-    return {
-        items: items,
-        totalItems: cachedItems.length
-    };
+const getItems = function(payload) {
+  const start = Number.parseInt(payload.start) || 0;
+  const limit = Number.parseInt(payload.limit) || 9;
+  const items = cachedItems.slice(start, start + limit);
+  let fileRawData = fs.readFileSync("./server/data/favoriteItemIds.json");
+  let favoriteItemIds = JSON.parse(fileRawData);
+  items.map(
+    e => (e.isFavorite = favoriteItemIds.indexOf(e.id) == -1 ? false : true)
+  );
+  return {
+    items: items,
+    totalItems: cachedItems.length
+  };
 };
 
-browseRouter.get('', (req, res)=>{
-    const response = getItems(req.query);
-    res.status(200).json(response);
+browseRouter.get("", (req, res) => {
+  const response = getItems(req.query);
+  res.status(200).json(response);
 });
 
 module.exports = browseRouter;
--- server/routes/itemRouter.js
+++ server/routes/itemRouter.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const cachedItems = require("../data/items.json");
-const fs = require('fs');
+const fs = require("fs");
 
 const itemRouter = express.Router();
 
 const getItem = function(itemId) {
-  let item =
-    cachedItems.find(function(item) {
-      return item.id === itemId || item.integerId === itemId;
-    });
-  if(item){
-  let fileRawData = fs.readFileSync("./server/data/favoriteItemIds.json");
-  let favoriteItemIds = JSON.parse(fileRawData);
-  item.isFavorite = favoriteItemIds.indexOf(itemId) == -1 ? false : true;
-  return item;
+  let item = cachedItems.find(function(item) {
+    return item.id === itemId || item.integerId === itemId;
+  });
+  if (item) {
+    let fileRawData = fs.readFileSync("./server/data/favoriteItemIds.json");
+    let favoriteItemIds = JSON.parse(fileRawData);
+    item.isFavorite = favoriteItemIds.indexOf(itemId) == -1 ? false : true;
+    return item;
   } else {
     return {};
   }
@@ -29,9 +28,12 @@ function setIsItemFavorite(itemId, isFavorite) {
   } else if (isFavorite == "false") {
     favoriteItemIds.splice(favoriteItemIds.indexOf(itemId), 1);
   }
-  console.log(isFavorite)
-  fs.writeFileSync("./server/data/favoriteItemIds.json", JSON.stringify(favoriteItemIds, null, 2));
-};
+  console.log(isFavorite);
+  fs.writeFileSync(
+    "./server/data/favoriteItemIds.json",
+    JSON.stringify(favoriteItemIds, null, 2)
+  );
+}
 
 itemRouter.get("/:id", (req, res) => {
   const id = req.params.id;
