6747fddc6ba19c2884f4e33e3c15d04731b4ca5b
Voidpumpkin <juliuslungys@gmail.com>
2019-06-16 19:34:43 +0300

    fix itemCard lint error

--- src/Browse/ItemCard.js
+++ src/Browse/ItemCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import "./ItemCard.css";
 import FavoriteButton from "../SharedComponents/FavoriteButton";
 
